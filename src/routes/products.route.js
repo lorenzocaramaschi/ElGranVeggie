f28334e7cc460ae4ca8bc0531d97da0723910485
Lorenzo <caramaschilolo@gmail.com>
2022-12-13 23:59:24 -0300

    Primera Entrega del Proyecto FInal

---
+++ src/routes/products.route.js
@@ -0,0 +1,115 @@
+import { Router } from "express";
+
+const productsRouter = Router();
+const products = [];
+
+productsRouter.get("/", (req, res) => {
+  res.render("productForm.ejs");
+});
+
+productsRouter.get("api/products/product", (req, res) => {
+  res.render("products.ejs",{products});
+});
+
+productsRouter.post("api/products/product", (req, res) => {  
+  const { name, price, thumbnail } = req.body;
+  products.push({name,price,thumbnail})
+
+  res.redirect("/")
+});
+
+productsRouter
+  .route("/")
+  .get((req, res) => {
+    const response = {
+      status: "ok",
+      data: products,
+    };
+
+    res.json(response);
+  })
+  .post((req, res) => {
+    const { title, price, thumbnail } = req.body;
+    const newProductId = products[products.length - 1].id + 1;
+    const newProduct = {
+      title,
+      price,
+      thumbnail,
+      id: newProductId,
+    };
+
+    const response = {
+      status: "Created Product",
+      data: newProduct,
+    };
+
+    products.push(newProduct);
+
+    res.status(201).json(response);
+  });
+
+productsRouter
+  .route("/:id")
+  .put((req, res) => {
+    const { id } = req.params;
+    const { title, price, thumbnail } = req.body;
+    const productToUpdateIndex = products.find(
+      (product) => product.id === Number(id)
+    );
+
+    if (!productToUpdateIndex) {
+      return res.status(404).json({ status: "Not found", data: null });
+    }
+
+    products.splice(productToUpdateIndex, 1, { id, title, price, thumbnail });
+
+    res.status(200).json({
+      status: "Updated!",
+      data: { id, title, price, thumbnail },
+    });
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const { title, price, thumbnail } = req.body;
+    const productToDeleteIndex = products.find(
+      (product) => product.id === Number(id)
+    );
+    const productToDelete = products[productToDeleteIndex];
+
+    if (!productToDeleteIndex) {
+      return res.status(404).json({ status: "Not found", data: null });
+    }
+
+    products.splice(productToDeleteIndex, 1);
+
+    res.status(200).json({
+      status: "Deleted!",
+      data: productToDelete,
+    });
+  })
+  .get((req, res) => {
+    const { id } = req.params;
+    const product = products.find((product) => product.id === Number(id));
+    let response;
+    let statusCode;
+
+    if (product) {
+      response = {
+        status: "ok",
+        data: product,
+      };
+
+      statusCode = 200;
+    } else {
+      response = {
+        status: "Not found",
+        data: null,
+      };
+
+      statusCode = 404;
+    }
+
+    res.status(statusCode).json(response);
+  });
+
+export default productsRouter;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,31 @@
-import express from "express";
+import express, {json,urlencoded} from "express";
 import { Server as IOServer } from "socket.io";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import moment from "moment";
+import productsRouter from "./routes/products.route.js";
 
+const PORT = 8080
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const expressServer = app.listen(8080, () => {
-  console.log("Server listening port 8080");
+const expressServer = app.listen(PORT, () => {
+  console.log(`Server listening port ${PORT}`);
 });
 const io = new IOServer(expressServer);
 const messages = [];
 
+app.use("/api/products",productsRouter)
 app.use(express.static(__dirname + "/public"));
+app.set('view engine','ejs')
+app.set('views',__dirname+"/views")
 app.get("/", (req, res) => {
-  res.render("index.html");
+  res.render("products.html");
 });
+app.use(json())
+app.use(urlencoded({ extended: true }));
+
+
+
 
 io.on("connection", (socket) => {
   console.log(
