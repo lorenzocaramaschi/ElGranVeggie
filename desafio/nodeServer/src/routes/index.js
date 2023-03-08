69c2eaac3499c7183289311cd73f917b5d52e5bc
Lorenzo <caramaschilolo@gmail.com>
2023-03-08 00:19:03 -0300

    Desafío: Loggers, gzip y análisis de performance

--- desafio/nodeServer/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const router = Router()
-router.get("/api/randoms", randomController.getRandom);
-router.get("/info", randomController.getInfo);
-import { randomController } from "../controllers/random.controller.js";
-
-export default router
